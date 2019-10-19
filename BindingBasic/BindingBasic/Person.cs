0c15669d3f8139bb85f245b6ab68d2b49710ec34
安然 张 <zar234@qq.com>
2019-10-19 21:13:19 +0800

    update

---
+++ BindingBasic/BindingBasic/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingBasic
+{
+    public class Person:INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+        //public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+
+        public Person() { }
+        public Person(string _name,int _age,string _sex)
+        {
+            Name = _name;
+            Age = _age;
+            Sex = _sex;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
