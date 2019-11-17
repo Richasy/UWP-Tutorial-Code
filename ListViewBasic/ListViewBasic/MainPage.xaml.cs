a55b47d6586aab952c7e954b7b7e2fe41887c434
安然 张 <zar234@qq.com>
2019-11-17 10:32:58 +0800

    添加ListView基础代码

---
+++ ListViewBasic/ListViewBasic/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace ListViewBasic
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var item = e.ClickedItem as string;
+            int tag = Convert.ToInt32(item.Replace("Scenario ",""));
+            switch (tag)
+            {
+                case 1:
+                    MainFrame.Navigate(typeof(Pages.Scenario_1));
+                    break;
+                case 2:
+                    MainFrame.Navigate(typeof(Pages.Scenario_2));
+                    break;
+                case 3:
+                    MainFrame.Navigate(typeof(Pages.Scenario_3));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
