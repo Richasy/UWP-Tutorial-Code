fb35366f0e87197591bcede762c597e264025310
Richasy <zar234@qq.com>
2020-04-03 19:15:22 +0800

    update

---
+++ ListView_DataTemplate/ListView_DataTemplate/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListView_DataTemplate
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<string> StringCollection = new ObservableCollection<string>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                StringCollection.Add(i.ToString());
+            }
+        }
+    }
+}
